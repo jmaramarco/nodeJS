3053eed2707428f8240cb9fb9a4f2e65f23c7ebb
jmaramarco <145615974+jmaramarco@users.noreply.github.com>
2024-05-29 09:35:09 -0300

    Aplicando Cambios al login

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,30 @@ document.addEventListener("DOMContentLoaded", function() {
     navUl.classList.toggle('open');
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var popup = document.getElementById('loginPopup');
+  var btn = document.getElementById('registroAtletas');
+  var span = document.getElementsByClassName('close')[0];
+
+  btn.onclick = function() {
+      popup.style.display = 'block';
+  }
+
+  span.onclick = function() {
+      popup.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+      if (event.target == popup) {
+          popup.style.display = 'none';
+      }
+  }
+
+  // Redirigir a formulario.html al hacer clic en "Crear cuenta"
+  var createAccountLink = document.getElementById('createAccountLink');
+  
+  createAccountLink.onclick = function() {
+      window.location.href = 'formulario.html';
+  }
+});
